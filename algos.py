c8e88840a785dbd9d5b1455e7b1e2ab3f5e8324e
Ahmadreza Rahimi <ahmadreza.rahimi@rub.de>
2023-04-12 17:42:19 +0200

    clean

--- algos.py
+++ algos.py
@@ -1,36 +1,50 @@
-from math import ceil,sqrt,log2
-from petrelic.multiplicative.pairing import G1,G2,GT,G1Element,G2Element,Bn
+from math import ceil, sqrt, log2
+from petrelic.multiplicative.pairing import G1, G2, GT, G1Element, G2Element, Bn
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
+from pathos.multiprocessing import Pool
+from loky import get_reusable_executor
+import threading
 
-def setup(n,L):
+def generate_arrays(params, results):
+    index, n, alpha, beta = params
+    n1 = n + 1
+
+    Ui = [G1.random() for _ in range(n1)]
+    Uri = [G2.random() for _ in range(n1)]
+
+    Ai = G1.random()
+    Bi = G2.random()
+    results[index] = (Ui, Uri, Ai, Bi)
+
+
+def setup(n, L):
+    alpha = Bn.random(G1.order())
+    beta = Bn.random(G1.order())
+    gamma = Bn.random(G1.order())
 
     g1 = G1.generator()
     g2 = G2.generator()
-
-    alpha = G1.order().random()
-    beta = G1.order().random()
-    gamma = G1.order().random()
-
-    Z = g1.pair(g2)**alpha
+    Z = g1.pair(g2) ** alpha
     h = g1 ** beta
     Gamma = g1 ** gamma
-    n1 = n+1
+
+    n1 = n + 1
     U = []
     A = []
     B = []
     W = []
     Ur = []
-    for i in range(L):
-        Ui = []
-        Uri = []
-        for w in range(n1):
-            Uri.append(G1.order().random())
-            Ui.append(g1 ** Uri[-1])
-        Ur.append(Uri)
+
+    executor = get_reusable_executor()
+    results = list(executor.map(generate_arrays, [(i, n, alpha, beta) for i in range(L)]))
+
+    for result in results:
+        Ui, Uri, Ai, Bi = result
         U.append(Ui)
-        t = G2.order().random()
-        Ai = g2 ** t
+        Ur.append(Uri)
         A.append(Ai)
-        B.append(g2**alpha * Ai**beta)
+        B.append(Bi)
 
     for i in range(L):
         Wi = []
@@ -38,43 +52,53 @@ def setup(n,L):
             Wj = []
             if i != j:
                 for w in range(n1):
-                    Wj.append(A[i] ** (Ur[i][w] * gamma.mod_pow(-1,G1.order())))
+                    Wj.append(A[i] ** (Ur[i][w] * gamma.mod_inverse(G1.order())))
             Wi.append(Wj)
         W.append(Wi)
-    crs = (g1,g2,Z,h,Gamma,A,B,U,W)
-    return crs
-def keyGen(crs,i,L,n):
-    #parse crs
-    g1,g2,Z,h,Gamma,A,B,U,W = crs
-    r = G1.order().random()
-    T = (U[i][-1] ** r) ** -1
-    Wh = []
-    for j in range(L):
-        if j!= i:
-            Wh.append(W[i][j][-1]**r)
-    pk = [T,Wh]
-    x = []
-    for i in range(n):
-        x.append(G1.order().random())
-    return pk,x
 
-def aggregate(crs,n,L,pks,xs):
+    crs = (g1, g2, Z, h, Gamma, A, B, U, W)
+    return crs
+
+
+
+# ... (the rest of the algos.py file remains the same)
+
+
+
+
+
+def keyGen(crs, i, L, n):
     g1, g2, Z, h, Gamma, A, B, U, W = crs
-    T = []
-    Wh = []
-    for i in range(L):
-        Ti = pks[i][0]
-        for w in range(n):
-            Ti *= U[i][w]** (-1 * xs[i][w])
-        T.append(Ti)
-    for j in range(L-1):
-        Whj = []
-        for i in range(L-1):
-            Whi = pks[j][1][i]
-            if i != j:
-                for w in range(n):
-                    # TODO the pow is wrong, -1 must be added
-                    Whi*= W[j][i][w]** (-1*xs[i][w])
-            Whj.append(Whi)
-        Wh.append(Whj)
+    order = G1.order()
+    r = order.random()
+    T = (U[i][-1] ** r) ** -1
+    Wh = [W[i][j][-1] ** r for j in range(L) if j != i]
+
+    pk = [T, Wh]
+    x = [order.random() for _ in range(n)]
+
+    return pk, x
+
+
+def aggregate(crs, n, L, pks, xs):
+    g1, g2, Z, h, Gamma, A, B, U, W = crs
+
+    T = [
+        pk[0] * product(U[i][w] ** (-1 * x) for w, x in enumerate(x_list))
+        for i, (pk, x_list) in enumerate(zip(pks, xs))
+    ]
+
+    Wh = [
+        [
+            pk[1][i] * product(W[j][i][w] ** (-1 * x) for w, x in enumerate(xs[j if j < i else j + 1]))
+            for i in range(L - 1)
+        ]
+        for j, pk in enumerate(pks)
+    ]
+
+def product(iterable):
+    result = G1.neutral_element()
+    for x in iterable:
+        result *= x
+    return result
 
--- main.py
+++ main.py
@@ -1,28 +1,127 @@
-from algos import *
 from time import time
+from petrelic.multiplicative.pairing import G1, G2, GT, G1Element, G2Element,GTElement, Bn
+from concurrent.futures import ThreadPoolExecutor
+import sqlite3
+
+def setup(n, L):
+    g1 = G1.generator()
+    g2 = G2.generator()
+
+    order = G1.order()
+    alpha = order.random()
+    beta = order.random()
+    gamma = order.random()
+    gamma_inv = gamma.mod_inverse(order)
+
+    Z = g1.pair(g2) ** alpha
+    h = g1 ** beta
+    Gamma = g1 ** gamma
+    n1 = n + 1
+
+    Ur = [[order.random() for _ in range(n1)] for _ in range(L)]
+    U = [[g1 ** uri for uri in ur] for ur in Ur]
+
+    A = [g2 ** order.random() for _ in range(L)]
+    alpha_beta = alpha * beta
+    B = [a ** alpha_beta for a in A]
+
+    def compute_W(i, j):
+        return [a ** (ur * gamma_inv) for a, ur in zip(A, Ur[i])]
+
+    with ThreadPoolExecutor() as executor:
+        W = [[executor.submit(compute_W, i, j) if i != j else [] for j in range(L)] for i in range(L)]
+
+    for i in range(L):
+        for j in range(L):
+            if i != j:
+                W[i][j] = W[i][j].result()
+
+    conn = sqlite3.connect("crs.db")
+    cur = conn.cursor()
+
+    # Create the tables
+    cur.execute('CREATE TABLE IF NOT EXISTS crs_g1 (id INTEGER PRIMARY KEY, value BLOB)')
+    cur.execute('CREATE TABLE IF NOT EXISTS crs_g2 (id INTEGER PRIMARY KEY, value BLOB)')
+    cur.execute('CREATE TABLE IF NOT EXISTS crs_gt (id INTEGER PRIMARY KEY, value BLOB)')
+    cur.execute('CREATE TABLE IF NOT EXISTS crs_h (id INTEGER PRIMARY KEY, value BLOB)')
+    cur.execute('CREATE TABLE IF NOT EXISTS crs_gamma (id INTEGER PRIMARY KEY, value BLOB)')
+    cur.execute('CREATE TABLE IF NOT EXISTS crs_A (id INTEGER PRIMARY KEY, value BLOB)')
+    cur.execute('CREATE TABLE IF NOT EXISTS crs_B (id INTEGER PRIMARY KEY, value BLOB)')
+    cur.execute('CREATE TABLE IF NOT EXISTS crs_U (i INTEGER, j INTEGER, value BLOB, PRIMARY KEY (i, j))')
+    cur.execute('CREATE TABLE IF NOT EXISTS crs_W (i INTEGER, j INTEGER, k INTEGER, value BLOB, PRIMARY KEY (i, j, k))')
+
+    # Store the CRS data in the database
+    cur.execute('INSERT OR REPLACE INTO crs_g1 (id, value) VALUES (?, ?)', (1, g1.to_binary()))
+    cur.execute('INSERT OR REPLACE INTO crs_g2 (id, value) VALUES (?, ?)', (1, g2.to_binary()))
+    cur.execute('INSERT OR REPLACE INTO crs_gt (id, value) VALUES (?, ?)', (1, Z.to_binary()))
+    cur.execute('INSERT OR REPLACE INTO crs_h (id, value) VALUES (?, ?)', (1, h.to_binary()))
+    cur.execute('INSERT OR REPLACE INTO crs_gamma (id, value) VALUES (?, ?)', (1, Gamma.to_binary()))
+
+    for i, a in enumerate(A):
+        cur.execute('INSERT OR REPLACE INTO crs_A (id, value) VALUES (?, ?)', (i, a.to_binary()))
+    for i, b in enumerate(B):
+        cur.execute('INSERT OR REPLACE INTO crs_B (id, value) VALUES (?, ?)', (i, b.to_binary()))
+
+    for i, row in enumerate(U):
+        for j, u in enumerate(row):
+            cur.execute('INSERT OR REPLACE INTO crs_U (i, j, value) VALUES (?, ?, ?)', (i, j, u.to_binary()))
+
+    for i, row in enumerate(W):
+        for j, col in enumerate(row):
+            for k, w in enumerate(col):
+                cur.execute('INSERT OR REPLACE INTO crs_W (i, j, k, value) VALUES (?, ?, ?, ?)', (i, j, k, w.to_binary()))
+
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+
+
+def load_crs(db_path='crs.db'):
+    conn = sqlite3.connect(db_path)
+    cur = conn.cursor()
+
+    cur.execute('SELECT value FROM crs_g1 WHERE id = 1')
+    g1 = G1Element.from_binary(cur.fetchone()[0])
+    cur.execute('SELECT value FROM crs_g2 WHERE id = 1')
+    g2 = G2Element.from_binary(cur.fetchone()[0])
+    cur.execute('SELECT value FROM crs_gt WHERE id = 1')
+    Z = GTElement.from_binary(cur.fetchone()[0])
+    cur.execute('SELECT value FROM crs_h WHERE id = 1')
+    h = G1Element.from_binary(cur.fetchone()[0])
+    cur.execute('SELECT value FROM crs_gamma WHERE id = 1')
+    Gamma = G1Element.from_binary(cur.fetchone()[0])
+
+    cur.execute('SELECT value FROM crs_A ORDER BY id')
+    A = [G2Element.from_binary(row[0]) for row in cur.fetchall()]
+    cur.execute('SELECT value FROM crs_B ORDER BY id')
+    B = [G2Element.from_binary(row[0]) for row in cur.fetchall()]
+
+    cur.execute('SELECT i, j, value FROM crs_U ORDER BY i, j')
+    U = [[] for _ in range(L)]
+    for row in cur.fetchall():
+        i, j, value = row
+        U[i].append(G1Element.from_binary(value))
+
+    cur.execute('SELECT i, j, k, value FROM crs_W ORDER BY i, j, k')
+    W = [[[] for _ in range(L)] for _ in range(L)]
+    for row in cur.fetchall():
+        i, j, k, value = row
+        if i!=j:
+            W[i][j].append(G2Element.from_binary(value))
+
+    conn.close()
+
+    crs = g1, g2, Z, h, Gamma, A, B, U,W
+    return crs
 
 
 L = 100
 n = 10
-# CRS gen time
-s = time()
-crs = setup(n,L)
-e = time()
-print("CRS gen time:",e-s)
-s = time()
-pks = []
-xs = []
-for i in range(L):
-    pk,x = keyGen(crs,i,L,n)
-    pks.append(pk)
-    xs.append(x)
-e = time()
-print("KeyGen time:",e-s)
 
-# print(pks[0][1][8])
+start_time = time()
+setup(n, L)
+g1, g2, Z, h, Gamma, A, B, U,W = load_crs("crs.db")
+elapsed_time = time() - start_time
+print("CRS gen time:", elapsed_time)
 
-s = time()
-aggregate(crs,n,L,pks,xs)
-e = time()
-print("Agg time:",e-s)
-
+print(W[0][0])
